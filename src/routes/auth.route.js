d930639f92af942fab4460d59176b60035e84584
Shoyeb45 <shoyebff45@gmail.com>
2024-12-29 15:27:28 +0530

    applied middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import Router from "express";
 import { isLoggedIn } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.route("/isLoggedIn").get(isLoggedIn);
+router.route("/isLoggedIn").get(verifyJWT, isLoggedIn);
 
 export default router;
